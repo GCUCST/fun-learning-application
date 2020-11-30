aed8975df82b1a3eba6d6d323bed32d04adb26e6
Shaotong chen <13144061737@163.com>
2020-12-01 02:05:03 +0800

    #delete comment
    Signed-off-by: Shaotong chen <13144061737@163.com>

--- src/main/java/cn/cst/main/MainApplication.java
+++ src/main/java/cn/cst/main/MainApplication.java
@@ -1,9 +1,6 @@
 package cn.cst.main;
 
 public class MainApplication {
-    //    public static void main(String[] args) {
-//        System.out.println("Hello world!");
-//    }
     public void output() {
         System.out.println("hello world!");
     }
