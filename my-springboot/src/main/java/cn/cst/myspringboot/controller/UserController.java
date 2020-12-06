c90e1fc3816564346a25032b36c7b20b5cea4a4e
Shaotong chen <13144061737@163.com>
2020-12-07 00:36:46 +0800

    # test autowired #20201206

--- my-springboot/src/main/java/cn/cst/myspringboot/controller/UserController.java
+++ my-springboot/src/main/java/cn/cst/myspringboot/controller/UserController.java
@@ -1,25 +1,73 @@
 package cn.cst.myspringboot.controller;
 
 import cn.cst.myspringboot.pojo.User;
+import cn.cst.myspringboot.service.UserService;
+import cn.cst.myspringboot.util.CommonResponse;
 import io.swagger.annotations.*;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.websocket.server.PathParam;
+import java.time.LocalTime;
 
 @Api(value = "用户controller", tags = {"用户操作接口"})
 @RestController
+@Slf4j
 public class UserController {
-    @ApiOperation(value = "获取用户信息", tags = {"获取用户信息——单独"}, notes = "实现笔记") //单独
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+
     @GetMapping("/getUser")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "username", value = "用户名称", dataType = "String", paramType = "query")})
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "succ", response = CommonResponse.class)})
+    public CommonResponse getUser(String username) {
+        return userService.getUser(username);
+    }
+
+    @GetMapping("/getAllUsers")
+    @ApiOperation("获取所有用户")
+    public CommonResponse getUser() {
+        log.info("get all users." + LocalTime.now());
+        return userService.getAllUser();
+    }
+
+    @GetMapping("/saveUser")
+    @ApiOperation("保存用户.")
+    public CommonResponse saveUser(String username) {
+        log.info("save user." + LocalTime.now());
+        return userService.saveUser(User.builder().username(username).password("123456").build());
+    }
+
+    @GetMapping("/deleteUser")
+    @ApiOperation("删除用户.")
+    public CommonResponse delUser(String username) {
+        log.info("del user." + LocalTime.now());
+        return userService.deleteUser(username);
+    }
+
+
+    @GetMapping("/updateUser")
+    @ApiOperation("更新密码用户.")
+    public CommonResponse updateUser(User user) {
+        log.info("del user." + LocalTime.now());
+        return userService.updateUser(user);
+    }
+
+    @ApiOperation(value = "获取用户信息", tags = {"获取用户信息——单独"}, notes = "实现笔记") //单独
+    @GetMapping("/getUser1")
     @ApiImplicitParams({
             @ApiImplicitParam(name = "username", value = "用户名称", dataType = "String", paramType = "query")})
     public User getUserInfo(@ApiParam(name = "username", value = "用户名", required = true, example = "老王") @PathParam("username") String username) {
         User user = new User();
-        user.password = "pass";
-        user.username = username;
         return user;
     }
 
