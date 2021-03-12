630d16edb9e7877a2f577ac24778cfd533017c8e
Shaotong chen <13144061737@163.com>
2021-03-12 22:21:25 +0800

    # 添加websocket

---
+++ hello-websocket/src/main/java/cn/cst/component/Config.java
@@ -0,0 +1,20 @@
+package cn.cst.component;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * 开启WebSocket支持
+ *
+ * @author zhengkai.blog.csdn.net
+ */
+@Configuration
+public class Config {
+
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+
+}
